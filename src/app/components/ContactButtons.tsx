f848b364ad32f16da3f341490f8596c87ef144c7
SkyDogzz <thomas.stephan@live.fr>
2024-01-22 14:26:38 +0000

    Refactor ContactButtons and Projects components

--- src/app/components/ContactButtons.tsx
+++ src/app/components/ContactButtons.tsx
@@ -4,28 +4,43 @@ import { FaGithub, FaLinkedin, FaEnvelope, FaDiscord } from "react-icons/fa";
 export default function ContactButtons() {
   return (
     <div className="flex gap-5">
-      <HeroButton url="https://github.com/skydogzz" icon={<FaGithub />} name="Github" />
-      <HeroButton url="https://www.linkedin.com/in/thomas-stephan-8376b6172/" icon={<FaLinkedin />} name="Linkedin" />
-      <HeroButton url="mailto:thomas.stephan@live.fr" icon={<FaEnvelope />} name="thomas.stephan@live.fr" />
-      <HeroButton url="https://discordapp.com/users/489883265595998227" icon={<FaDiscord />} name="Discord" />
+      <HeroButton
+        url="https://github.com/skydogzz"
+        icon={<FaGithub />}
+        name="Github"
+      />
+      <HeroButton
+        url="https://www.linkedin.com/in/thomas-stephan-8376b6172/"
+        icon={<FaLinkedin />}
+        name="Linkedin"
+      />
+      <HeroButton
+        url="mailto:thomas.stephan@live.fr"
+        icon={<FaEnvelope />}
+        name="thomas.stephan@live.fr"
+      />
+      <HeroButton
+        url="https://discordapp.com/users/489883265595998227"
+        icon={<FaDiscord />}
+        name="Discord"
+      />
     </div>
   );
 }
 
 interface HeroButtonProps {
-    url: string;
-    icon: React.ReactNode;
-    name: string;
-  }
-  
-  function HeroButton({ url, icon, name }: HeroButtonProps) {
-    return (
-      <Link href={url} target="_blank">
-        <button className="flex gap-2 items-center bg-emerald-600 py-1 px-3 rounded-full">
-          {icon}
-          {name}
-        </button>
-      </Link>
-    );
-  }
-  
+  url: string;
+  icon: React.ReactNode;
+  name: string;
+}
+
+function HeroButton({ url, icon, name }: HeroButtonProps) {
+  return (
+    <Link href={url} target="_blank">
+      <button className="flex gap-2 items-center bg-emerald-600 py-1 px-3 rounded-full hover:scale-110 transition-all">
+        {icon}
+        {name}
+      </button>
+    </Link>
+  );
+}
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,9 +1,71 @@
 import { Roboto } from "next/font/google";
+import React from "react";
 const roboto = Roboto({ subsets: ["latin"], weight: "700" });
+import { FaReact, FaLink } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Projects() {
-  return (<section id="projects" className="skills flex flex-col justify-center gap-10 pb-20">
-  <h1 className={roboto.className + " text-4xl text-center"}>Projects</h1>
+  return (
+    <section
+      id="projects"
+      className="skills flex flex-col justify-center gap-10 pb-20"
+    >
+      <h1 className={roboto.className + " text-4xl text-center"}>Projects</h1>
+      <div className="cards grid gap-10 grid-cols-3">
+        <Project
+          name="Portfolio"
+          description="This Portfolio"
+          link="/"
+          icon={<FaReact />}
+          techs={["Reactjs", "Nextjs", "Tailwindcss", "Sass"]}
+        />{" "}
+        <Project
+          name="Portfolio"
+          description="This Portfolio"
+          link="/"
+          icon={<FaReact />}
+          techs={["Reactjs", "Nextjs", "Tailwindcss", "Sass"]}
+        />{" "}
+        <Project
+          name="Portfolio"
+          description="This Portfolio"
+          link="/"
+          icon={<FaReact />}
+          techs={["Reactjs", "Nextjs", "Tailwindcss", "Sass"]}
+        />
+      </div>
     </section>
+  );
+}
+
+interface ProjectProps {
+  name: string;
+  description: string;
+  link: string;
+  icon: React.ReactNode;
+  techs: string[];
+}
+
+function Project({ name, description, link, icon, techs }: ProjectProps) {
+  return (
+    <div className="card rounded-2xl bg-emerald-900 min-w-52 p-4 border border-emerald-300 hover:scale-110 transition-all">
+      <div className="cardHeader flex justify-between">
+        {icon}
+        <Link href={link}>
+          <FaLink />
+        </Link>
+      </div>
+      <div className="cardBody pt-5">
+        <h2 className={roboto.className + " text-2xl"}>{name}</h2>
+        <p>{description}</p>
+        <div className="badges flex flex-wrap gap-2 pt-1">
+          {techs.map((tech) => (
+            <span className="badge bg-emerald-700 hover:bg-emerald-600 px-2 py-1 rounded-xl gap-2">
+              {tech}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
