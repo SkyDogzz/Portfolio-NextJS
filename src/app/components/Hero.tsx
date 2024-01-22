7cc0f0eb09d2d768b7fcf936492c7d73043b29a5
SkyDogzz <thomas.stephan@live.fr>
2024-01-22 14:47:44 +0000

    Add typeit-react library for typing animation

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,17 +1,24 @@
+"use client";
 import { Roboto } from "next/font/google";
 const roboto = Roboto({ subsets: ["latin"], weight: "700" });
 import ContactButtons from "./ContactButtons";
+import TypeIt from "typeit-react";
 
 export default function Hero() {
   return (
     <section
-      className={roboto.className + " hero flex flex-col justify-center gap-10 pb-20"}
+      className={
+        roboto.className + " hero flex flex-col justify-center gap-10 pb-20"
+      }
     >
-      <h1 className=" text-emerald-400 text-xl">
-        Hey There! I'm -<br /> <span className="text-8xl">Thomas Stephan</span>
-      </h1>
-      <p className="text-3xl">I'm a Fullstack Web Developer</p>
-      <ContactButtons/>
+      <TypeIt options={{speed:10}}>
+        <h1 className=" text-emerald-400 text-xl">
+          Hey There! I'm -<br />{" "}
+          <span className="text-8xl">Thomas Stephan</span>
+        </h1>
+        <p className="text-3xl">I'm a Fullstack Web Developer</p>
+      </TypeIt>
+      <ContactButtons />
     </section>
   );
 }
