2ea02d59387b941b24fa00650d4af54b5e14138d
SkyDogzz <thomas.stephan@live.fr>
2024-01-22 13:48:09 +0000

    Update contact button URLs and add target="_blank"

--- src/app/components/ContactButtons.tsx
+++ src/app/components/ContactButtons.tsx
@@ -4,10 +4,10 @@ import { FaGithub, FaLinkedin, FaEnvelope, FaDiscord } from "react-icons/fa";
 export default function ContactButtons() {
   return (
     <div className="flex gap-5">
-      <HeroButton url="/" icon={<FaGithub />} name="Github" />
-      <HeroButton url="/" icon={<FaLinkedin />} name="Linkedin" />
-      <HeroButton url="/" icon={<FaEnvelope />} name="thomas.stephan@live.fr" />
-      <HeroButton url="/" icon={<FaDiscord />} name="Discord" />
+      <HeroButton url="https://github.com/skydogzz" icon={<FaGithub />} name="Github" />
+      <HeroButton url="https://www.linkedin.com/in/thomas-stephan-8376b6172/" icon={<FaLinkedin />} name="Linkedin" />
+      <HeroButton url="mailto:thomas.stephan@live.fr" icon={<FaEnvelope />} name="thomas.stephan@live.fr" />
+      <HeroButton url="https://discordapp.com/users/489883265595998227" icon={<FaDiscord />} name="Discord" />
     </div>
   );
 }
@@ -20,7 +20,7 @@ interface HeroButtonProps {
   
   function HeroButton({ url, icon, name }: HeroButtonProps) {
     return (
-      <Link href={url}>
+      <Link href={url} target="_blank">
         <button className="flex gap-2 items-center bg-sky-600 py-1 px-3 rounded-full">
           {icon}
           {name}
