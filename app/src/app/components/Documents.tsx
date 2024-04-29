1e9b057d479e5d11dcc1562bcdf30d1a40ebb3d0
SkyDogzz <thomas.stephan@live.fr>
2024-04-29 02:36:28 +0000

    Refactor Documents component and add Button component for document links

--- app/src/app/components/Documents.tsx
+++ app/src/app/components/Documents.tsx
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google";
 const roboto = Roboto({ subsets: ["latin"], weight: "700" });
+import Link from "next/link";
 
 export default function Documents() {
     return (
@@ -10,22 +11,35 @@ export default function Documents() {
             }
         >
             <h1 className=" text-4xl text-center">Documents</h1>
-            <div className="flex flex-col gap-5">
-                <a
-                    href="/documents/Resume.pdf"
-                    target="_blank"
-                    className="btn btn-primary"
-                >
-                    Resume
-                </a>
-                <a
-                    href="/documents/assessfirst_softskills.pdf"
-                    target="_blank"
-                    className="btn btn-primary"
-                >
-                    Soft Skills Assessfirst
-                </a>
+            <div className="flex justify-center gap-5">
+                <Button
+                    url="/documents/Resume.pdf"
+                    icon={<i className="fas fa-file-pdf"></i>}
+                    name="Resume"
+                />
+                <Button
+                    url="/documents/assessfirst_softskills.pdf"
+                    icon={<i className="fas fa-file-pdf"></i>}
+                    name="Soft Skills Assessfirst"
+                />
             </div>
         </section>
+    );
+}
+
+type ButtonProps = {
+    url: string;
+    icon: JSX.Element;
+    name: string;
+};
+
+function Button({ url, icon, name }: ButtonProps) {
+    return (
+        <Link href={url} target="_blank">
+            <button className="flex gap-2 items-center bg-emerald-600 py-1 px-3 rounded-full sm:hover:scale-110 transition-all">
+                {icon}
+                {name}
+            </button>
+        </Link>
     );
 }
